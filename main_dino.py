b5fa02b53589d418f49b1c6c30e97b38fb9613af
sflaviu <sflaviu96@gmail.com>
2022-10-13 18:43:10 +0300

    Remove check for XCIT architecture to fix multi-GPU training with other architectures

--- main_dino.py
+++ main_dino.py
@@ -167,11 +167,11 @@ def train_dino(args):
         teacher = vits.__dict__[args.arch](patch_size=args.patch_size)
         embed_dim = student.embed_dim
     # if the network is a XCiT
-    elif args.arch in torch.hub.list("facebookresearch/xcit:main"):
-        student = torch.hub.load('facebookresearch/xcit:main', args.arch,
-                                 pretrained=False, drop_path_rate=args.drop_path_rate)
-        teacher = torch.hub.load('facebookresearch/xcit:main', args.arch, pretrained=False)
-        embed_dim = student.embed_dim
+    # elif args.arch in torch.hub.list("facebookresearch/xcit:main"):
+    #     student = torch.hub.load('facebookresearch/xcit:main', args.arch,
+    #                              pretrained=False, drop_path_rate=args.drop_path_rate)
+    #     teacher = torch.hub.load('facebookresearch/xcit:main', args.arch, pretrained=False)
+    #     embed_dim = student.embed_dim
     # otherwise, we check if the architecture is in torchvision models
     elif args.arch in torchvision_models.__dict__.keys():
         student = torchvision_models.__dict__[args.arch]()
