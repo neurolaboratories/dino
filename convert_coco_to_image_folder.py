3df584dc0af0eacff4e007a128ffb0ba15858087
LDP <ldp@192-168-0-100.rdsnet.ro>
2023-02-10 10:51:57 +0200

    add coco file name

--- convert_coco_to_image_folder.py
+++ convert_coco_to_image_folder.py
@@ -15,36 +15,38 @@ args = parser.parse_args()
 
 path_to_coco = Path(args.coco_dataset_path)
 
-coco_json_paths = list(path_to_coco.iterdir())
+# coco_json_paths = list(path_to_coco.iterdir())
+final_coco_path = None
+for elem in list(path_to_coco.iterdir()):
+    if ".json" in str(elem):
+        final_coco_path = elem
 output_folder = "train"
 
 dataset_ouptut_path = Path(args.image_folder_output_path)
 
-for coco_json_path in coco_json_paths:
+with open(final_coco_path, "r") as coco_file:
+    coco_json = json.load(coco_file)
 
-    with open(coco_json_path, "r") as coco_file:
-        coco_json = json.load(coco_file)
+category_id_to_name_dict = {}
+for category in coco_json["categories"]:
+    category_id_to_name_dict[category["id"]] = category["name"]
 
-    category_id_to_name_dict = {}
-    for category in coco_json["categories"]:
-        category_id_to_name_dict[category["id"]] = category["name"]
+image_id_to_name_dict = {}
+for image in coco_json["images"]:
+    image_id_to_name_dict[image["id"]] = image["file_name"]
 
-    image_id_to_name_dict = {}
-    for image in coco_json["images"]:
-        image_id_to_name_dict[image["id"]] = image["file_name"]
+for annotation in coco_json["annotations"]:
+    coco_bbox = annotation["bbox"]
 
-    for annotation in coco_json["annotations"]:
-        coco_bbox = annotation["bbox"]
+    if coco_bbox[2] == 0.0 or coco_bbox[3] == 0.0:
+        continue
 
-        if coco_bbox[2] == 0.0 or coco_bbox[3] == 0.0:
-            continue
+    category_name = category_id_to_name_dict[annotation["category_id"]]
+    image_filename = Path(image_id_to_name_dict[annotation["image_id"]])
 
-        category_name = category_id_to_name_dict[annotation["category_id"]]
-        image_filename = Path(image_id_to_name_dict[annotation["image_id"]])
+    image_crop = Image.open(path_to_coco / "images" / image_filename).crop(
+        (coco_bbox[0], coco_bbox[1], coco_bbox[0] + coco_bbox[2], coco_bbox[1] + coco_bbox[3])
+    )
 
-        image_crop = Image.open(path_to_coco / "images" / image_filename).crop(
-            (coco_bbox[0], coco_bbox[1], coco_bbox[0] + coco_bbox[2], coco_bbox[1] + coco_bbox[3])
-        )
-
-        (dataset_ouptut_path / output_folder / 'object').mkdir(parents=True, exist_ok=True)
-        image_crop.save(dataset_ouptut_path / output_folder / 'object' / (category_name + "_" + str(annotation["id"]) + image_filename.suffix))
+    (dataset_ouptut_path / output_folder / 'object').mkdir(parents=True, exist_ok=True)
+    image_crop.save(dataset_ouptut_path / output_folder / 'object' / (category_name + "_" + str(annotation["id"]) + image_filename.suffix))
